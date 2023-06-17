a51f55315b4f737bcad9eccc55f554f6bae7040e
minipol <ninevoso@gmail.com>
2023-06-17 10:28:25 +0300

    обновление программы : 1.изминился интерфейс (кнопки) 2.закрепился способ открытия картинок 3.к изменёным кнопкам был добавлен функционал (кнопка "вперёд>>>" показывает следущее иэображение, кнопка "назад<<<" показывает предедущее изображение)

--- picture.py
+++ picture.py
@@ -5,47 +5,47 @@ from PIL import ImageTk, Image
 
 window = Tk()
 window.title('картинки')
-window.attributes('-fullscreen',True)
 
 # настраиваем цвет фона
 
-StonesLabel = Label(window)
-StonesLabel.pack()
-KolbosaLabel = Label(window)
-KolbosaLabel.pack()
-SaulLabel = Label(window)
-SaulLabel.pack()
+photoLabel = Label(window)
+photoLabel.pack()
 
-# создаём картинки
+# создаём функции, которые открывают картинку и запихивает в label
 
-def StonesClick():
-    StonesPhoto = PhotoImage(file="D:/nikit_OS/picture/rock.png")
-    StonesLabel.configure(image=StonesPhoto, width=1000, height=500)
-    StonesLabel.image = StonesPhoto
-
-def KolbosaClick():
-    KolbosaPhoto = ImageTk.PhotoImage(Image.open('D:/nikit_OS/picture/kolbosa.png'))
-    StonesLabel.configure(image=KolbosaPhoto,width=1000,height=500)
-    StonesLabel.image = KolbosaPhoto
-
-def SaulClick():
-    SaulPhoto = ImageTk.PhotoImage(Image.open('D:/nikit_OS/picture/saul goodman.png'))
-    StonesLabel.configure(image=SaulPhoto,width=1000,height=500)
-    StonesLabel.image = SaulPhoto
+current_name_picture = 1
+namePicture = f"D:/nikit_OS/picture/pctr/{current_name_picture}.png"
+photoPicture = ImageTk.PhotoImage(Image.open(namePicture))
+photoLabel.configure(image=photoPicture, width=1000, height=500)
+photoLabel.image = photoPicture
 
 # создаём текст в окне
 
 WelcomeLabel = Label(window, text='нажми на любую кнопку')
 WelcomeLabel.pack()
 
+def nextPhoto():
+    global current_name_picture
+    current_name_picture = current_name_picture + 1
+    namePicture = f'D:/nikit_OS/picture/pctr/{current_name_picture}.png'
+    photoPicture = ImageTk.PhotoImage(Image.open(namePicture))
+    photoLabel.configure(image=photoPicture,width=1000,height=500)
+    photoLabel.image = photoPicture
+
+def backPhoto():
+    global current_name_picture
+    current_name_picture = current_name_picture - 1
+    namePicture = f'D:/nikit_OS/picture/pctr/{current_name_picture}.png'
+    photoPicture = ImageTk.PhotoImage(Image.open(namePicture))
+    photoLabel.configure(image=photoPicture,width=1000,height=500)
+    photoLabel.image = photoPicture
+
 # привязываем кнопки к картинкам (строка 22,27,32)
 
-StonesButton = Button(window, text='камни', command=StonesClick)
-StonesButton.pack()
-KolbosaButton = Button(window,text='деловая колбоса',command=KolbosaClick)
-KolbosaButton.pack()
-SaulButton = Button(window,text='человек',command=SaulClick)
-SaulButton.pack()
+btn = Button(text='вперёд>>>',command=nextPhoto)
+btn.pack()
+btn1 = Button(text='<<<назад',command=backPhoto)
+btn1.pack()
 
 # цикл окна
 
